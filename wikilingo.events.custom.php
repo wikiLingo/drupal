694d61fe6209de4164a27163beeb5bed565629e9
Robert Plummer <robertleeplummerjr@gmail.com>
2014-03-24 16:46:56 -0400

    Give a custom event handler

---
+++ wikilingo.events.custom.php
@@ -0,0 +1,12 @@
+<?php
+
+class wikiLingoEventsCustom
+{
+  public function wikiLingoEventsCustom(WikiLingo\Events &$events)
+  {
+    $events->bind(new WikiLingo\Event\Expression\Variable\Lookup(function() {
+
+
+    }));
+  }
+}
