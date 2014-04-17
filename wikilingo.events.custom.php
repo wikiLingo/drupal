8337be5cf6cf28cadc65de0da2b4fd953bacf575
Robert Plummer <robertleeplummerjr@gmail.com>
2014-04-17 09:45:32 -0400

    Setup permission plugin, fix paths.

--- wikilingo.events.custom.php
+++ wikilingo.events.custom.php
@@ -4,9 +4,31 @@ class wikiLingoEventsCustom
 {
   public function wikiLingoEventsCustom(WikiLingo\Events &$events)
   {
-    $events->bind(new WikiLingo\Event\Expression\Variable\Lookup(function() {
+    $events
+        ->bind(new WikiLingo\Event\Expression\Variable\Lookup(function() {
 
 
-    }));
+        }))
+
+        ->bind(new WikiLingo\Event\Expression\Plugin\PreRender(function(WikiLingo\Expression\Plugin &$plugin) {
+            global $user;
+
+            switch (strtolower($plugin->type)) {
+                case "permission":
+                    $role = $plugin->parameter("role");
+                    if (!empty($role)) {
+                        if (in_array($role, $this->roles)) {
+                            $plugin->class->permissible = true;
+                        }
+                    }
+            }
+        }))
+
+        ->bind(new WikiLingo\Event\Expression\Plugin\PostRender(function(&$rendered, WikiLingo\Expression\Plugin &$plugin) {
+            switch (strtolower($plugin->type)) {
+                case "permission":
+                    $plugin->class->permissible = false;
+            }
+        }));
   }
 }
